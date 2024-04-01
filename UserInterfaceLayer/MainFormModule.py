780960020ee81891f83e18389c00e41087102615
shima1364 <sh.movafagh.474@gmail.com>
2024-04-01 23:09:32 +0200

    9

--- UserInterfaceLayer/MainFormModule.py
+++ UserInterfaceLayer/MainFormModule.py
@@ -4,71 +4,74 @@ from .AuthorsFormModule import AuthorFormClass
 from .SalesTable import SaleFormClass
 from .TitleFormModule import TitleFormClass
 from .StorFormModule import StorFormClass
-
+from tkinter import ttk
 
 class MainFormClass:
     def mainFormLoad(self, fname, lname):
         mainFormObject = Tk()
         mainFormObject.title('Main Form')
-        mainFormObject.geometry('400x400')
+        mainFormObject.geometry('500x600')
         mainFormObject.iconbitmap('images/mainIcon.ico')
         mainFormObject.resizable(0, 0)
+
+        # Calculate the center position
         x = int(mainFormObject.winfo_screenwidth() / 2 - 400 / 2)
         y = int(mainFormObject.winfo_screenheight() / 2 - 400 / 2)
         mainFormObject.geometry('+{}+{}'.format(x, y))
 
+        # Function to center the window
+        def center_window(window):
+            window.update_idletasks()
+            width = window.winfo_width()
+            height = window.winfo_height()
+            x_pos = (window.winfo_screenwidth() // 2) - (width // 2)
+            y_pos = (window.winfo_screenheight() // 2) - (height // 2)
+            window.geometry('{}x{}+{}+{}'.format(width, height, x_pos, y_pos))
+
+        # Apply custom styles to buttons
+        style = ttk.Style()
+        style.configure("Main.TButton", font=("Helvetica", 12), foreground="black", background="#d9d9d9", padx=20, pady=10)
+
         def employeeFormLoad():
-          #  mainFormObject.destroy()
             employeeFormObject = EmployeeFormClass()
             employeeFormObject.employeeCRUDFormload()
 
         def authorFormLoad():
-           # mainFormObject.destroy()
             authorFormObject = AuthorFormClass()
             authorFormObject.authorFormload()
 
         def salesFormLoad():
-          #  mainFormObject.destroy()
             salesFormObject = SaleFormClass()
             salesFormObject.salesFormload()
 
         def titleFormLoad():
-          #  mainFormObject.destroy()
             titleFormObject = TitleFormClass()
             titleFormObject.TitleFormload()
 
         def storFormLoad():
-          #  mainFormObject.destroy()
             storFormObject = StorFormClass()
             storFormObject.storFormload()
 
-
-
-        # def employeeTableLoad():
-        # mainFormObject.destroy()
-        # Create a new window or frame to display the retrieved data
-        #    employeeTableObject = EmployeeTableClass()
-        # Example: Create a new window
-        #   employeeTableWindow = Toplevel(employeeTableObject)
-        #  employeeTableWindow.title('Employee Table')
-        # Add code to display the retrieved data in the new window
-
         lblWelcomeMessage = Label(mainFormObject, text=f'Welcome {fname} {lname}')
-        lblWelcomeMessage.grid(row=0, column=1, padx=10, pady=10)
+        lblWelcomeMessage.grid(row=0, padx=10, pady=10, columnspan=2)
+        employeeCRUD = PhotoImage(file='images/Employee_icon.png')
+        btnEmployeeCRUD = ttk.Button(mainFormObject, text='Employee CRUD', command=employeeFormLoad, style="Main.TButton", image=employeeCRUD)
+        btnEmployeeCRUD.grid(row=1, column=0, padx=20, pady=20, sticky='we')
+        author_icon = PhotoImage(file='images/author_icon.png')
+        btnAuthorCRUD = ttk.Button(mainFormObject, text='Author CRUD', command=authorFormLoad, style="Main.TButton", image=author_icon)
+        btnAuthorCRUD.grid(row=1, column=1, padx=20, pady=20, sticky='we')
+        sale_Icon = PhotoImage(file='images/Sales_icon.png')
+        btnSales = ttk.Button(mainFormObject, text='Sales', command=salesFormLoad, style="Main.TButton", image=sale_Icon)
+        btnSales.grid(row=2, column=0, padx=20, pady=20, sticky='we')
+        title_Icon = PhotoImage(file='images/Titles_icon.png')
+        btnTitle = ttk.Button(mainFormObject, text='Title', command=titleFormLoad, style="Main.TButton",image=title_Icon)
+        btnTitle.grid(row=2, column=1, padx=20, pady=20, sticky='we')
+        store_Icon = PhotoImage(file='images/Store_icon.png')
+        btnStor = ttk.Button(mainFormObject, text='Stor', command=storFormLoad, style="Main.TButton", image=store_Icon)
+        btnStor.grid(row=3, column=0, padx=20, pady=20, columnspan=2, sticky='we')
 
-        btnEmployeeCRUD = Button(mainFormObject, text='Employee CRUD', command=employeeFormLoad)
-        btnEmployeeCRUD.grid(row=1, column=0, padx=20, pady=20)
+        # Center the window after widgets are added
+        center_window(mainFormObject)
 
-        btnAuthorCRUD = Button(mainFormObject, text='Author CRUD', command=authorFormLoad)
-        btnAuthorCRUD.grid(row=2, column=0, padx=20, pady=20)
-
-        btnSales = Button(mainFormObject, text='Sales', command=salesFormLoad)
-        btnSales.grid(row=3, column=0, padx=20, pady=20)
-
-        btnTitle = Button(mainFormObject, text='Title', command=titleFormLoad)
-        btnTitle.grid(row=4, column=0, padx=20, pady=20)
-
-        btnStor = Button(mainFormObject, text='Stor', command=storFormLoad)
-        btnStor.grid(row=5, column=0, padx=20, pady=20)
         mainFormObject.mainloop()
 
